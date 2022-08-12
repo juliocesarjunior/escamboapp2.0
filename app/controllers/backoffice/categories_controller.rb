e1e158d648021ea1bcbe5d413742fce25fff7d56
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2022-08-12 17:14:15 -0300

    Administradores

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,18 +1,45 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
+
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(params_category)
+    if @category.save
+      redirect_to backoffice_categories_path 
+      flash[:success] =  "A categoria #{@category.description} foi criado com Sucesso."
+    else
+      render :new
+      flash[:alert] = "A categoria não foi cadastrado!"
+    end
   end
 
   def edit
   end
 
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path
+      flash[:success] = "A categoria #{@category.description} foi atualizado com Sucesso."
+    else
+      render :edit
+      flash[:alert] = "A categoria não foi atualizado!"
+    end
   end
   
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def params_category
+    params.require(:category).permit(:description)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+	#Verifica se estar em branco descrição
+	validates_presence_of :description
 end
